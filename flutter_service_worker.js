7f28a759bd966310c7919c4b2e34b78636ede9c8
VISHNU S.L <vishnu@butomy.in>
2024-03-01 16:22:54 +0530

    update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "0c869aed90bccfe4b040fa30ddc21f74",
-"index.html": "76fe592565694d87cd1d66e73209b2d0",
-"/": "76fe592565694d87cd1d66e73209b2d0",
-"main.dart.js": "c333b4eedc0333d7a261be19e93bd6c4",
+"index.html": "6c8cc7ada4ea4d327469fd73030243aa",
+"/": "6c8cc7ada4ea4d327469fd73030243aa",
+"main.dart.js": "c37080032d4f81ef9181acbcc2327acb",
 "flutter.js": "c71a09214cb6f5f8996a531350400a9a",
 "favicon.png": "67fdf50ec8a90b997f217d783c6a8168",
 "icons/Icon-192.png": "81f73813fe1f757dfd6e6d267debd8cf",
